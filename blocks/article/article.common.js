b7fd3cfb7f684f6e6cf7c60c4201c68446c848d9
vovkvlad <VKolo.bokV@gmail.com>
2014-07-16 13:58:07 +0300

    bugs in article block fixed

--- blocks/article/article.common.js
+++ blocks/article/article.common.js
@@ -1,9 +1,10 @@
 BN.addDecl('article').blockTemplate(function(ctx){
-    var article = ctx.json().currentarticle;
+    var article = ctx.json().currentarticle[0];
+    console.log(article);
     ctx.content([
-        {elem: title, content: article.title},
-        {elem: line},
-        {elem: text, content: article.content}
+        {elem: 'title', content: article.title},
+        {elem: 'line'},
+        {elem: 'text', content: article.content}
     ]);
 }).elemTemplate({
         title: function(ctx){
--- blocks/article/article.priv.js
+++ blocks/article/article.priv.js
@@ -1,6 +1,6 @@
 BN.addDecl('article').dataTemplate(function(ctx){
     var nid = ctx.param('newsid');
-    return BN('presa-api').get('/'+nid).then(function(data){
+    return BN('pressa-api').get(nid).then(function(data){
         ctx.param('currentarticle', data);
     });
 });
