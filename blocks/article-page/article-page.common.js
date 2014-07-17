b1b81bcd611a6361f65d50668dded60f35b73dbd
vovkvlad <VKolo.bokV@gmail.com>
2014-07-17 13:29:09 +0300

    pages in footer left

--- blocks/article-page/article-page.common.js
+++ blocks/article-page/article-page.common.js
@@ -1,13 +1,3 @@
 BN.addDecl('article-page', 'page', {
     route: /^\/[0-9]*$/
-}).staticProp({
-        init: function(matches){
-            return this.out({block: 'main-container', newsid: matches[0].slice(1)});
-        },
-        update: function(){
-
-        },
-        destruct: function(){
-
-        }
-    });
+});
---
+++ blocks/article-page/article-page.js
@@ -0,0 +1,11 @@
+BN('article-page').staticProp({
+    init: function(){
+        return vow.reject();
+    },
+    update: function(){
+
+    },
+    destruct: function(){
+
+    }
+});
