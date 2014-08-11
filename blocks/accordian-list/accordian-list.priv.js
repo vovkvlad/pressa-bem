4eab721efd7d9d2b9fd46323f98a5300a5c0bb17
vovkvlad <VKolo.bokV@gmail.com>
2014-08-11 15:47:08 +0300

    categories a bit changed

--- blocks/accordian-list/accordian-list.priv.js
+++ blocks/accordian-list/accordian-list.priv.js
@@ -4,7 +4,7 @@ BN.addDecl('accordian-list').blockTemplate(function(ctx){
     ctx.content([
         {block : 'accordian-to-the-main-page', item_title: 'To the main page'},
         {block : 'accordian-list-item', item_title: 'Students Life', category: 'studentslife'},
-        {block : 'accordian-list-item', item_title: 'News', category: 'News'},
+        {block : 'accordian-list-item', item_title: 'News', category: 'news'},
         {block : 'accordian-list-item', item_title: 'Conferences', category: 'conferences'}
     ]);
 });
